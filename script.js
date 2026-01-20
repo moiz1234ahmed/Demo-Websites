3e4ce4b7545f17813cb13441eea979a6675208fd
moiz1234ahmed <1531moiz@gmail.com>
2026-01-20 22:00:04 +0500

    js DOM

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-console.log("this is strings tutorial");
-let a = "harry";
-console.log(a[0]);
-console.log(a[1]);
-console.log(a[2]);
-console.log(a[3]);
-console.log(a[4]);
-console.log(a.length);
-let name = "harry";
-let freind = "moiz";
-console.log("his name is" + name + "and their freind is" + freind);
-console.log(`his name is ${name} and their freind is ${freind}`);
-let b = "Moizahmed";
-console.log(b.toUpperCase());
-console.log(b.toLowerCase());
-console.log(b.slice(1, 6));
-// console.log(b.replace("moiz" , "123"))
-console.log(b.concat(a, "wdawada"));
+document.body.childNodes;
