82083152f5c2043d628bc0ffb413c7f919e791a9
moiz1234ahmed <1531moiz@gmail.com>
2026-01-20 19:55:16 +0500

    Java script 2

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-alert("Hello moiz");
-console.log("code is running")
-console.log("code is processing")
-console.log("code is stop")
-
-var a= prompt("Enter your number=")
-var istrue=confirm("Are you sure you want to blast your computer")
-if(istrue){
-    console.log("computer is blasting")
-}
-else{
-    console.log("computer is not blasting")
-
-
-}
-console.log("Your number is" +  a)
-document.title=("Java script")
-document.body.style.backgroundColor="blue"
+alert("Hey Moiz");
+var a = 5;
+var b = 90;
+console.log(a + b);
+console.log(typeof a, typeof b);
