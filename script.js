016213efc3d80bd19245addae24f323044f5ae1e
moiz1234ahmed <1531moiz@gmail.com>
2026-01-20 20:53:12 +0500

    js strings

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-alert("Hey Moiz");
-var a = 5;
-var b = 90;
-console.log(a + b);
-console.log(typeof a, typeof b);
+console.log("this is strings tutorial");
+let a = "harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a.length);
+let name = "harry";
+let freind = "moiz";
+console.log("his name is" + name + "and their freind is" + freind);
+console.log(`his name is ${name} and their freind is ${freind}`);
+let b = "Moizahmed";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(1, 6));
+// console.log(b.replace("moiz" , "123"))
+console.log(b.concat(a, "wdawada"));
